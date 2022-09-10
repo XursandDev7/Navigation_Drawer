8784cd25b1bb2c4b14665f8edabdfcc9d9764472
jony335412 <qwertymonstar335412@gmail.com>
2022-09-10 13:37:56 +0500

    mansurni qoshdim

--- app/src/main/java/com/astro/navigation_drawer/MainActivity.java
+++ app/src/main/java/com/astro/navigation_drawer/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     String phone="";
     String gmail="";
     String imageurl="";
+    String mansur="Negap";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
